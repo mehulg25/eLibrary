fd91c8c4a0a2ce197f79bc38a584f72c8af8117c
Mehul Gupta <mehulg25@gmail.com>
2021-04-12 11:48:53 +0530

    Bug Fixes

--- client/src/components/searchPage.js
+++ client/src/components/searchPage.js
@@ -1,53 +1,73 @@
-import React,{useState,useEffect} from 'react';
-import {useParams} from 'react-router-dom';
-import {useBooksState,useBooksDispatch,loadAllBooks,updateAllBooks} from '../BooksContext';
-import BookList from './bookList';
-import Axios from 'axios';
-import {useUserState} from '../UserContext';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  useBooksState,
+  useBooksDispatch,
+  loadAllBooks,
+  updateAllBooks,
+} from "../BooksContext";
+import BookList from "./bookList";
+import Axios from "axios";
+import { useUserState } from "../UserContext";
+import { useHistory } from "react-router-dom";
 
 function SearchBooks() {
+  const { allBooks } = useBooksState();
+  const { searchText } = useParams();
+  const booksDispatch = useBooksDispatch();
+  const [allBooksSet, setAllBooksSet] = useState(false);
+  const { isAuthenticated, user } = useUserState();
+  const history = useHistory();
 
-    const {allBooks} = useBooksState();
-    const {searchText} = useParams();
-    const booksDispatch = useBooksDispatch();
-    const [allBooksSet, setAllBooksSet] = useState(false);
-    const {isAuthenticated, user} = useUserState();
+  useEffect(() => {
+    if (isAuthenticated) {
+      Axios.get("/allBooks.php")
+        .then((response) => {
+          var filteredBooks = response.data.books.filter((book) => {
+            if (
+              book.name.toLowerCase().indexOf(searchText.toLowerCase()) !==
+                -1 ||
+              book.author_name
+                .toLowerCase()
+                .indexOf(searchText.toLowerCase()) !== -1
+            )
+              return book;
+          });
 
-    useEffect(() => {
+          response.data.books = filteredBooks;
+          if (response.data.books != undefined) {
+            response.data.books.map((book) => {
+              book.book_id = book.id;
+              book.isBookIssued = false;
+              book.isBookBookmarked = false;
+              book.isBookRead = false;
+            });
 
-        if (isAuthenticated) {
-            Axios.get("/allBooks.php").then((response) => {
+            loadAllBooks(booksDispatch, response);
+            setAllBooksSet(true);
+          }
+        })
+        .catch((err) => console.log(err));
+      // setCurrentlyIssuedBookId(user.currently_issued_bookid);
+    }
+  }, [user, searchText]);
 
-                var filteredBooks = response.data.books.filter(book=>{
-                    if(book.name.toLowerCase().indexOf(searchText) !== -1 || book.author_name.toLowerCase().indexOf(searchText) !== -1)
-                        return book;
-                })
-
-                response.data.books = filteredBooks
-                if (response.data.books != undefined) {
-                    response.data.books.map(book => {
-                        book.book_id = book.id
-                        book.isBookIssued = false;
-                        book.isBookBookmarked = false;
-                        book.isBookRead = false;
-
-                    })
-
-                    loadAllBooks(booksDispatch,response)
-                    setAllBooksSet(true)
-                }
-            }).catch(err => console.log(err));
-            // setCurrentlyIssuedBookId(user.currently_issued_bookid);
-        }
-    }, [user,searchText]); 
-
-
-    return(
-        <div>{console.log('hello')}
-        <BookList books={allBooks} styleClass="expandedList"/></div>
-
-    )
-
+  return (
+    <div>
+      {console.log("hello")}
+      <p>Search Results For : {searchText}</p>
+      <button
+        type="button"
+        className="close"
+        onClick={() => {
+          history.push("/dashboard");
+        }}
+      >
+        X
+      </button>
+      <BookList books={allBooks} styleClass="expandedList" />
+    </div>
+  );
 }
 
 export default SearchBooks;
